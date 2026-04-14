66353e85163f2001dc34677c96367dc879f8e699
ryzencatz <ryzencatz@gmail.com>
2026-04-14 17:10:28 +0000

    added js stuff

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-// add javascript here
+let acc = document.getElementsByClassName("accordion");
+
+for (let i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        // Toggle 'active' on the parent div (.section) 
+        // This allows one class to trigger both the dash and the panel
+        this.parentElement.classList.toggle("active");
+    
+    });
+}
